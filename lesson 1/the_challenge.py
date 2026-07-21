d31adee1528d34409ec49063badb855b31de020f
Thabiso Nkambule <nkambulethabiso88@gmail.com>
2026-07-21 23:03:17 +0200

    Create the_challenge.py

---
+++ lesson 1/the_challenge.py
@@ -0,0 +1,6 @@
+# Creating a concert ticket booker
+
+name = input("Enter your name: ")
+artist = input("Enter artist you want to see: ")
+
+print(f"Hey {name}! Your tickets to see {artist} are booked successfully!")
