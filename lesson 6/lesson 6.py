00e9fdc54758675f71d037e11d552e5b3c18e259
Thabiso Nkambule <nkambulethabiso88@gmail.com>
2026-07-22 21:45:58 +0200

    Create lesson 6.py

---
+++ lesson 6/lesson 6.py
@@ -0,0 +1,17 @@
+# Working list (Shopping cart)
+
+cart = ["Apples ," "Eggs ," "Milk"]
+print(cart[0])
+
+cart.append("Bread")
+print(cart)
+
+# Working with dictionaries 
+student = {
+    "name" : "Thabiso" ,
+    "age"  : 21,
+    "course" : "Intro to Python"
+
+}
+
+print(student["name"])
